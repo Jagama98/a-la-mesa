3a4ba963a6a41dc99bd9ff0ef57c20a8deb67a36
Javier Garcia <javiergarciamartin424@gmail.com>
2021-05-31 12:45:10 +0200

    Update Plato.js

--- src/models/Plato.js
+++ src/models/Plato.js
@@ -4,9 +4,9 @@ const { Schema } = mongoose;
 const PlatoSchema = new Schema({
     restaurante: { type: String, required: true},
     plato: { type: String, required: true},
-    cantidad: { type: String, required: true},
+    cantidad: { type: Number, required: true},
     direccion: { type: String, required: true},
-    telefono: { type: String, required: true},
+    telefono: { type: Number, required: true},
     date: { type: Date, default: Date.now}
 });
 
